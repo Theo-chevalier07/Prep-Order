0d6db3d95d0f7ca4b0aa1ee37d7502b0cab91110
Theo <theo.chevalier@campuscarloacutis.fr>
2025-12-09 11:32:48 +0100

    <hotfix> 8 conflit

--- Prep'Order/Prep'Order/obj/Debug/net8.0-windows/Prep'Order.AssemblyInfo.cs
+++ Prep'Order/Prep'Order/obj/Debug/net8.0-windows/Prep'Order.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prep\'Order")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be716b28f4c5d391e8aabff84961f6fc802e910b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5da4880e32d9dc36693b5d2a2429180cb1325b5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prep\'Order")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prep\'Order")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
