0e6313974de39de15c3c6a4fbb06caa1367df1f4
Theo <theo.chevalier@campuscarloacutis.fr>
2025-12-15 23:55:02 +0100

    <features> chargement csv et page gestion utilisateur

--- Prep'Order/Prep'Order/Commande.cs
+++ Prep'Order/Prep'Order/Commande.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -24,12 +25,46 @@ namespace Prep_Order
 
         private void Commande_Load(object sender, EventArgs e)
         {
-            string connectionString = "Server=JULIEN;Database=bdd_prep-order;Trusted_Connection=True;TrustServerCertificate=True;";
-            string query = "SELECT produitID, nomProduit, Quantite, nomZone FROM [Produit] inner join [Zone] on Zone.zoneID = produit.zoneID inner join [Stock] on stock.produitID = produit.produitID";
-            
-
-
         }
 
+        private void bt_reparateur_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fichierCsv = new OpenFileDialog()
+            {
+                Title = "Selectionner un fichier CSV",
+                Filter = "Fichier CSV (*.csv)|*.csv",
+                Multiselect = false,
+            };
+
+            if (fichierCsv.ShowDialog() == DialogResult.OK)
+            {
+                lv_objet.Items.Clear();
+
+                string[] lignes = File.ReadAllLines(fichierCsv.FileName);
+
+                for (int i = 1; i < lignes.Length; i++)
+                {
+                    string[] valeurs = lignes[i].Split(',');
+
+                    if (valeurs.Length == 4)
+                    {
+                        ListViewItem item = new ListViewItem(valeurs[0]); // ID
+                        item.SubItems.Add(valeurs[1]); // Nom
+                        item.SubItems.Add(valeurs[2]); // Quantité
+                        item.SubItems.Add(valeurs[3]); // Zone
+
+                        lv_objet.Items.Add(item);
+                    }
+                }
+            }
+        }
+
+        private void bt_assignerReparateur_Click(object sender, EventArgs e)
+        {
+            responsable_reparateur nouvellePage = new responsable_reparateur();
+            nouvellePage.Show();
+            this.Hide();
+        }
     }
 }
+
--- Prep'Order/Prep'Order/responsable_reparateur.Designer.cs
+++ Prep'Order/Prep'Order/responsable_reparateur.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Prep_Order
 {
-    partial class Commande
+    partial class responsable_reparateur
     {
         /// <summary>
         /// Required designer variable.
@@ -36,11 +36,7 @@
             lv_objet = new ListView();
             columnHeader1 = new ColumnHeader();
             columnHeader2 = new ColumnHeader();
-            columnHeader3 = new ColumnHeader();
             columnHeader4 = new ColumnHeader();
-            bt_reparateur = new Button();
-            ofd_csv = new OpenFileDialog();
-            bt_assignerReparateur = new Button();
             menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
@@ -66,7 +62,6 @@
             afficherToolStripMenuItem.Name = "afficherToolStripMenuItem";
             afficherToolStripMenuItem.Size = new Size(144, 26);
             afficherToolStripMenuItem.Text = "Afficher";
-            afficherToolStripMenuItem.Click += afficherToolStripMenuItem_Click;
             // 
             // utilisateursToolStripMenuItem
             // 
@@ -83,11 +78,11 @@
             // 
             // lv_objet
             // 
-            lv_objet.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3, columnHeader4 });
-            lv_objet.Location = new Point(144, 118);
+            lv_objet.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader4 });
+            lv_objet.Location = new Point(200, 94);
             lv_objet.Name = "lv_objet";
-            lv_objet.Size = new Size(466, 207);
-            lv_objet.TabIndex = 2;
+            lv_objet.Size = new Size(380, 207);
+            lv_objet.TabIndex = 3;
             lv_objet.UseCompatibleStateImageBehavior = false;
             lv_objet.View = View.Details;
             // 
@@ -100,52 +95,21 @@
             columnHeader2.Text = "Nom";
             columnHeader2.Width = 150;
             // 
-            // columnHeader3
-            // 
-            columnHeader3.Text = "quantités";
-            columnHeader3.Width = 100;
-            // 
             // columnHeader4
             // 
             columnHeader4.Text = "Zone";
             columnHeader4.Width = 150;
             // 
-            // bt_reparateur
-            // 
-            bt_reparateur.Location = new Point(285, 67);
-            bt_reparateur.Name = "bt_reparateur";
-            bt_reparateur.Size = new Size(175, 29);
-            bt_reparateur.TabIndex = 3;
-            bt_reparateur.Text = "Ajouter un fichier CSV";
-            bt_reparateur.UseVisualStyleBackColor = true;
-            bt_reparateur.Click += bt_reparateur_Click;
-            // 
-            // ofd_csv
-            // 
-            ofd_csv.FileName = "openFileDialog1";
-            // 
-            // bt_assignerReparateur
-            // 
-            bt_assignerReparateur.Location = new Point(285, 373);
-            bt_assignerReparateur.Name = "bt_assignerReparateur";
-            bt_assignerReparateur.Size = new Size(169, 29);
-            bt_assignerReparateur.TabIndex = 4;
-            bt_assignerReparateur.Text = "Assigner un réparateur";
-            bt_assignerReparateur.UseVisualStyleBackColor = true;
-            bt_assignerReparateur.Click += bt_assignerReparateur_Click;
-            // 
-            // Commande
+            // responsable_reparateur
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(bt_assignerReparateur);
-            Controls.Add(bt_reparateur);
             Controls.Add(lv_objet);
             Controls.Add(menuStrip1);
-            Name = "Commande";
-            Text = "Commande";
-            Load += Commande_Load;
+            Name = "responsable_reparateur";
+            Text = "responsable_reparateur";
+            Load += responsable_reparateur_Load;
             menuStrip1.ResumeLayout(false);
             menuStrip1.PerformLayout();
             ResumeLayout(false);
@@ -162,10 +126,6 @@
         private ListView lv_objet;
         private ColumnHeader columnHeader1;
         private ColumnHeader columnHeader2;
-        private ColumnHeader columnHeader3;
         private ColumnHeader columnHeader4;
-        private Button bt_reparateur;
-        private OpenFileDialog ofd_csv;
-        private Button bt_assignerReparateur;
     }
 }
